885355c7e8ada9a95baccb04270ec19ba921691d
avinash-lokani <avinash.lokani@zemosolabs.com>
2022-06-15 14:42:59 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useAuth0 } from '@auth0/auth0-react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginPage from './Components/LoginPage';
+import {PostList} from './Components/PostForm';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const {isAuthenticated, isLoading} = useAuth0();
+  if(isLoading){
+    return <div>Loading...</div>
+  }
+  if(isAuthenticated){
+    return (
+      <BrowserRouter>
+        <Routes>
+    
+          <Route path='/' element={<PostList/>} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }else{
+    return <LoginPage/>
+  }
 }
 
 export default App;
